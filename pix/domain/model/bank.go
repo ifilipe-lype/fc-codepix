2d8d0b45149e0d95af15bd136061a7ea67d9743f
ifilipe-lype <ifilipe.lype@gmail.com>
2024-03-30 10:42:41 +0100

    chore: exports Bank model

---
+++ pix/domain/model/bank.go
@@ -0,0 +1,37 @@
+package model
+
+import (
+	"time"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+type Bank struct {
+	Base     `valid:"required"`
+	Code     string     `json:"code" valid:"notnull"`
+	Name     string     `json:"name" valid:"notnull"`
+	Accounts []*Account `valid:"-"`
+}
+
+func (bank *Bank) validate() error {
+	_, err := govalidator.ValidateStruct(bank)
+
+	return err
+}
+
+func NewBank(code string, name string) (*Bank, error) {
+	bank := Bank{
+		Code: code,
+		Name: name,
+	}
+
+	bank.ID = uuid.NewV4().String()
+	bank.CreatedAt = time.Now()
+
+	if err := bank.validate(); err != nil {
+		return nil, err
+	}
+
+	return &bank, nil
+}
